df388e39b67fb8555c320708a098802e32f3aa73
qcx761 <3216563979@qq.com>
2025-03-13 21:45:39 +0800

    1

---
+++ 1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/wait.h>
+#include<string.h>
+#include<unistd.h>
+#include<signal.h>
+#include<fcntl.h>
+
+
+sigemptyset(&set);
+sigaddset(&set, SIGINT);
+pthread_sigmask(SIG_BLOCK, &set, NULL);
