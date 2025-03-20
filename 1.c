45189837056375bb23311fbd9b3c78c522a1351f
qcx761 <3216563979@qq.com>
2025-03-20 21:28:18 +0800

    1

--- 1.c
+++ 1.c
@@ -9,10 +9,6 @@
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER; // 一次性初始化互斥锁
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;   // 一次性初始化条件变量
 
-
-
-
-
 sigemptyset(&set);
 sigaddset(&set, SIGINT);
 pthread_sigmask(SIG_BLOCK, &set, NULL);
--- 2.c
+++ 2.c
@@ -48,7 +48,6 @@ int main() {
     return 0;
 }
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
