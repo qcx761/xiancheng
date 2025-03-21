4d04fda94795d0796b94690010afc1a5ed7d6b68
qcx761 <3216563979@qq.com>
2025-03-21 20:45:12 +0800

    1

--- 1.c
+++ 1.c
@@ -6,8 +6,12 @@
 #include<signal.h>
 #include<fcntl.h>
 
+
+
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER; // 一次性初始化互斥锁
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;   // 一次性初始化条件变量
+
+
 
 sigemptyset(&set);
 sigaddset(&set, SIGINT);
