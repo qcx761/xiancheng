f15309503e5b2208977050736a8eb2d3d2972fc0
qcx761 <3216563979@qq.com>
2025-03-25 22:00:08 +0800

    1

--- threadpool1/threadpool.hpp
+++ threadpool1/threadpool.hpp
@@ -4,53 +4,11 @@
 using namespace std;
 class threadpool {
     public:
-    threadpool(size_t num):stop(false){
-        for(int i=0;i<num;i++){
-            this->workers.emplace_back([this]{
-                while(1){
-                    function<void()> task;
-                    {
-                    unique_lock<mutex> lock(this->mtx);
-                    this->condition.wait(lock,[this]{return !this->tasks.empty()||this->stop;});
-                    if(stop&&this->tasks.empty()) return;
-                    task=this->tasks.front();
-                    this->tasks.pop();
-                    }
-                    task();
-                }
-            });
-        }
-    }
-    ~threadpool(){
-        {
-            unique_lock<mutex> lock(this->mtx);
-            this->stop=true;
-        }
-        this->condition.notify_all();
-        for(int j=0;j<this->workers.size();j++){
-            workers[j].join();
-        }
-    }
+        threadpool(size_t num);//线程池创建
+        ~threadpool(); // 释放
     
-    template<typename F,class... Args>
-auto enqueue(F&& f,Args&&... args) -> future<invoke_result_t<F,Args...>>{
-    using return_type=invoke_result_t<F,Args...>;
-     // 绑定函数和参数，创建packaged_task
-    auto task=make_shared<packaged_task<return_type()>>(bind(forward<F>(f),forward<Args>(args)...));
-    future<return_type> res=task->get_future();
-    {
-        unique_lock<mutex> lock(this->mtx);
-
-        if(this->stop) {
-            throw std::runtime_error("enqueue on stopped ThreadPool");
-        }
-
-        // 将任务包装为void()类型
-        this->tasks.emplace([task]{(*task)();});
-    }
-    this->condition.notify_one();
-    return res;
-}
+        template<typename F,class... Args> // 可变参数的 enqueue 版本
+        auto enqueue(F&& f,Args&&... args) -> future<invoke_result_t<F,Args...>>; // 创建任务队列
     
     private:
         vector<thread> workers; // 线程池中的线程
@@ -77,3 +35,8 @@ auto enqueue(F&& f,Args&&... args) -> future<invoke_result_t<F,Args...>>{
         // parameters：参数列表，类似于函数的参数。
         // -> return_type：可选，指定返回类型（通常可以省略，编译器会进行推导）。
         // {}：函数体。
+
+//enqueue 方法中使用了 lambda 表达式作为任务，lambda 表达式的类型是局部的（即在 main 函数中定义的），所以在 enqueue 方法中引用它时会导致编译器无法推导它的返回类型。
+//???
+//确保了 enqueue 函数的定义直接在 threadpool.hpp 头文件中。
+// 由于模板函数的实例化需要在编译时完成，所有模板的实现都必须在其声明处可见。因此，我将 enqueue 函数的实现搬到了头文件中，而不是放在 .cpp 文件中。
