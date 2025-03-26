6663a520b0e2cbc11117386355c1b72e79b16c2d
qcx761 <3216563979@qq.com>
2025-03-26 21:05:41 +0800

    1

--- threadpool1/main.cpp
+++ threadpool1/main.cpp
@@ -4,3 +4,19 @@ int main(){
     threadpool(4);
     return 0;
 }
+
+
+
+// void printMessage(void* arg) {
+//     char* message = static_cast<char*>(arg); // 将 void* 转换为 char*
+//     std::cout << message << std::endl;
+// }
+
+// int main() {
+//     char message[] = "Hello from a normal function!";
+//     Task task(printMessage, message);
+//     // 这里可以直接调用 task.function(task.arg) 来执行任务
+//     task.function(task.arg);
+//     return 0;
+// }
+//函数传参
--- threadpool1/threadpool.hpp
+++ threadpool1/threadpool.hpp
@@ -3,18 +3,31 @@ using namespace std;
 
 class Task{
     public:
-
-}
+        function<void(void*)> function;
+        void *arg;
+        Task(std::function<void(void*)> func,void* args):function(func),arg(args){};
+};
 
 class threadpool{
     public:
         threadpool(int num):stop(false){
-            
-
-
-
-
-
+            for(int i=0;i<num;i++){
+                this->workers.emplace_back([this]{
+                    while(1){
+                        Task task(nullptr,nullptr);
+                        {
+                        unique_lock<mutex> lock(this->mtx);
+                        this->condition.wait(lock,[this]{return !this->tasks.empty()||this->stop;});
+                        if(stop&&this->tasks.empty()) return;
+                        task=std::move(this->tasks.front());
+                        this->tasks.pop();
+                        }
+                        if(task.function){
+                            task.function(task.arg);// 执行函数
+                        }
+                    }
+                });
+            }
         }
         ~threadpool(){
 
@@ -22,13 +35,20 @@ class threadpool{
 
 
 
-            
+
         }
+    void Add_task(function<void(void*)> func,void* args){
+        {
+            unique_lock<mutex> lock(this->mtx);
+            tasks.emplace(Task(func,args))
+        }
+        condition.notify_one(); // 通知一个线程
+    }
     private:
     vector<thread> workers; // 线程池中的线程
     queue<Task> tasks; // 任务队列
     condition_variable condition; // 条件变量
     mutex mtx; // 互斥量
     bool stop; // 停止标志
-}
+};
 
