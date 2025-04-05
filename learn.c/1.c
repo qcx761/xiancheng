70f14d33b612d5389612187ea3c07d09323fbc96
qcx761 <3216563979@qq.com>
2025-04-05 13:47:50 +0800

    1

--- learn.c/1.c
+++ learn.c/1.c
@@ -11,6 +11,11 @@
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER; // 一次性初始化互斥锁
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;   // 一次性初始化条件变量
 
+pthread_mutex_t lock; // 声明互斥锁
+pthread_mutex_init(&lock, NULL); // 动态初始化互斥锁
+
+pthread_mutex_destroy(&lock);
+
 
 
 sigemptyset(&set);
